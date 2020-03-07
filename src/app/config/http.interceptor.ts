b927d9c14e726ea5d89ca91cd8ba414c0d6e9920
Abhinandan <abhi.kk747@gmail.com>
2020-03-07 23:39:41 +0530

    Updated angular to v9

--- src/app/config/http.interceptor.ts
+++ src/app/config/http.interceptor.ts
@@ -2,8 +2,10 @@ import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpResponse }
 	from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from './../../environments/environment';
+import { Injectable } from "@angular/core";
 
 
+@Injectable()
 export class WeatherHttpInterceptor implements HttpInterceptor {
 	intercept(
 		req: HttpRequest<any>,
